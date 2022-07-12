2790289623d9d3491eec3b9a8ec9a088530f0eb3
GyanendraChaubey <gyanendrachaubey68@gmai.com>
2022-07-12 15:41:32 +0530

    logging and exception

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import sys
+from storesales.exception import StoresalesException
+from storesales.logger import logging
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 
 def index():
+    try:
+       raise Exception("We are testing custom exception")
+    except Exception as e:
+        storesales = StoresalesException(e,sys)
+        logging.info(storesales.error)
+        logging.info("We are testing logging")
     return "CI CD Pipeline has been established"
 
 if __name__=="__main__":
