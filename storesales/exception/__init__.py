b3abb4a4b1f7efc17d8825dfbb69a57850d2499d
GyanendraChaubey <gyanendrachaubey68@gmai.com>
2022-07-13 01:02:02 +0530

    exception updated

--- storesales/exception/__init__.py
+++ storesales/exception/__init__.py
@@ -1,31 +1,30 @@
-from fileinput import filename
 import os
 import sys
 
-class StoresalesException(Exception):
-
-    def __init__(self, error_meassge:Exception, error_detail:sys):
-        super().__init__(error_meassge)
-        self.error_message=StoresalesException.get_detailed_error_message(error_meassge=error_meassge,
-        error_detail=error_detail)
-
+class HousingException(Exception):
     
+    def __init__(self, error_message:Exception,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
+                                                                       error_detail=error_detail
+                                                                        )
+
+
     @staticmethod
     def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
         """
-        error_meassge: Exception object
-
+        error_message: Exception object
         error_detail: object of sys module
         """
-
-        _,_,exec_tb = error_detail.exc_info()
-        line_number=exec_tb.tb_frame.f_lineno
-        file_name=exec_tb.tb_frame.f_code.co_filename
-        error_message=f"Error occurred in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
+        _,_ ,exec_tb = error_detail.exc_info()
+        line_number = exec_tb.tb_frame.f_lineno
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        error_message = f"Error occured in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
         return error_message
 
     def __str__(self):
         return self.error_message
 
+
     def __repr__(self) -> str:
-        return StoresalesException().__name__.str()
+        return HousingException.__name__.str()
