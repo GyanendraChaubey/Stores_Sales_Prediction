0043bd8b334e577b41f3294a3a95cb08da634aea
GyanendraChaubey <gyanendrachaubey68@gmai.com>
2022-07-26 15:27:04 +0530

    data validation completed

--- demo.py
+++ demo.py
@@ -1,15 +1,18 @@
-from storesales import pipeline
 from storesales.pipeline.pipeline import Pipeline
 from storesales.exception import StoresalesException
 from storesales.logger import logging
-
+from storesales.config.configuration import Configuration
 def main():
     try:
-        pipeline=Pipeline()
+        pipeline = Pipeline()
         pipeline.run_pipeline()
+        # data_validation_config = Configuartion().get_data_validation_config()
+        # print(data_validation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
+
+
 
 if __name__=="__main__":
     main()
--- storesales/pipeline/pipeline.py
+++ storesales/pipeline/pipeline.py
@@ -3,9 +3,10 @@ from tkinter import E
 from storesales.exception import StoresalesException
 from storesales.logger import logging
 import sys, os
-from storesales.entity.artifact_entity import DataIngestionArtifact
-from storesales.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig
+from storesales.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from storesales.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from storesales.component.data_ingestion import DataIngestion
+from storesales.component.data_validation import DataValidation
 from storesales.config.configuration import Configuration
 
 
@@ -24,11 +25,14 @@ class Pipeline:
         except Exception as e:
             raise StoresalesException(e,sys) from e
     
-    def start_data_validation(self):
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact)-> DataValidationArtifact:
         try:
-            pass
+            data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),
+                                             data_ingestion_artifact=data_ingestion_artifact
+                                             )
+            return data_validation.initiate_data_validation()
         except Exception as e:
-            raise StoresalesException(e,sys) from e
+            raise StoresalesException(e, sys) from e
     
     def start_data_transformation(self):
         try:
@@ -57,5 +61,7 @@ class Pipeline:
     def run_pipeline(self):
         try:
             data_ingestion_artfact=self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artfact)
+        
         except Exception as e:
             raise StoresalesException(e,sys) from e
